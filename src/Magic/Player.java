febd33a5c5e7924ee3c25f354ef3e2e5b5ea98bd
Enrico <Enrico@Enrico-PC>
2017-03-13 11:42:40 +0100

    Aggiunto metodo per controllare se il giocatore ha istantanei da giocare. Aggiunta la scelta della composizione del mazzo. Cambiato il mazzo da stack a lista (per comodità di shuffeling). Ora si lavora con Card e non più con liste di integer.

--- src/Magic/Player.java
+++ src/Magic/Player.java
@@ -1,9 +1,16 @@
 package Magic;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Random;
 import java.util.Stack;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.Random;
 
 /**
  * Rappresnets a player
@@ -25,12 +32,12 @@ public class Player {
     /**
      * The deck
      */
-    private Stack<Integer> mazzo = new Stack<>();
+    private List<Card> mazzo = new ArrayList<>();
 
     /**
      * The hand
      */
-    protected List<Integer> mano = new ArrayList<>();
+    protected List<Card> mano = new ArrayList<>();
 
     /**
      * The round handler
@@ -50,13 +57,23 @@ public class Player {
     /**
      * The player magics
      */
-    private List<Integer> magics = new ArrayList<>();
+    private List<Card> magics = new ArrayList<>();
 
     /**
      * The player monsters
      */
-    private List<Integer> monsters = new ArrayList<>();
-
+    private List<Card> monsters = new ArrayList<>();
+    
+    /**
+     * The input stream
+     */
+    private InputStreamReader reader;
+    
+    /**
+     * The buffer reader
+     */
+    private BufferedReader myInput;
+    
     /**
      * Creates a new player
      *
@@ -66,8 +83,8 @@ public class Player {
     public Player(String name, Playground playground) {
         this.name = name;
         this.playground = playground;
-        creaMazzo();
-        creaMano();
+        reader = new InputStreamReader(System.in);
+        myInput = new BufferedReader(reader);
     }
 
     /**
@@ -77,8 +94,8 @@ public class Player {
      */
     public Player(Playground playground) {
         this.playground = playground;
-        creaMazzo();
-        creaMano();
+        reader = new InputStreamReader(System.in);
+        myInput = new BufferedReader(reader);
     }
 
     // <editor-fold defaultstate="collapsed" desc="Getters & Setters">
@@ -106,46 +123,58 @@ public class Player {
         ingioco = boneggio;
     }
 
-    public List<Integer> getMagics() {
+    public List<Card> getMagics() {
         return magics;
     }
 
-    public void addMagic(Integer magic) {
+    public void addMagic(Card magic) {
         this.magics.add(magic);
     }
 
-    public void addMagics(List<Integer> magics) {
+    public void addMagics(List<Card> magics) {
         this.magics.addAll(magics);
     }
 
-    public List<Integer> getMonsters() {
+    public List<Card> getMonsters() {
         return monsters;
     }
 
-    public void addMonster(Integer monster) {
+    public void addMonster(Card monster) {
         this.monsters.add(monster);
     }
 
-    public void addMonster(List<Integer> monsters) {
+    public void addMonster(List<Card> monsters) {
         this.monsters.addAll(monsters);
     }
 
     // </editor-fold>
     
     // <editor-fold defaultstate="collapsed" desc="Deck handlers">
-    private void creaMazzo() {
+    protected void creaMazzo() {
         //DA IMPLEMENTARE IL FATTO CHE L'UTENTE DEBBA CREARSI IL MAZZO DI CARTE
-        Random rand = new Random();
-        int randomInt;
-        for (int i = 0; i < 20; i++) {
-            randomInt = rand.nextInt(20 - 1 + 1) + 1;
-            mazzo.push(randomInt);
-        }
+        int i=0;
+        int carta=0;
+        do{
+            do{
+                System.out.println(this.getNome() + " seleziona la carta " + (i+1) + " da inserire nel mazzo (presente solo omeophaty)");
+                System.out.println("1 - Omeophaty");
+                try {
+                    System.out.print("-> ");
+                    carta = Integer.parseInt(myInput.readLine());
+                } catch (IOException ex) {
+                    System.out.println("ahiahiaahi");
+                }
+            }while(carta != 1);
+            i++;
+            mazzo.add(new Instant("Omeophaty","Omeophaty does nothing"));
+        }while(i<20);
+        System.out.println("Mescolando");
+        Collections.shuffle(mazzo);
     }
 
     public void stampaMazzo() {
         for (int i = 0; i < mazzo.size(); i++) {
-            System.out.print(mazzo.get(i) + " ");
+            System.out.print(mazzo.get(i).getName() + " ");
         }
     }
 
@@ -153,7 +182,8 @@ public class Player {
         if (mazzo.isEmpty()) {
             ingioco = false;
         } else {
-            mano.add(mazzo.pop());
+            mano.add(mazzo.get(mazzo.size()-1));
+            mazzo.remove(mazzo.size()-1);
         }
     }
 
@@ -167,26 +197,60 @@ public class Player {
     // </editor-fold>
 
     // <editor-fold defaultstate="collapsed" desc="Hand handlers">
-        private void creaMano() {
+    protected void creaMano() {
         for (int i = 0; i < 5; i++) {
-            mano.add(mazzo.pop());
+            mano.add(mazzo.get(mazzo.size()-1));
+            mazzo.remove(mazzo.size()-1);
         }
     }
 
     public void stampaMano() {
         for (int i = 0; i < mano.size(); i++) {
-            System.out.print(mano.get(i) + " ");
+            System.out.print(mano.get(i).getName() + " ");
         }
     }
-
+    
+    /**
+     *Check if the player can and would use istant's card, otherwise don't do nothing
+     * Return ttue: if the player use an istans, false if don't want to play some istants
+     */
+    public boolean checkIstantToPlay(){
+        System.out.println("Seleziona l'istantaneo da giocare (-1 per uscire).");
+        for (int i = 0; i < mano.size(); i++) {
+            System.out.print(mano.get(i).getName() + " " + mano.get(i).getType());
+        }
+        int carta=0;
+        do{
+            try {
+                System.out.print("-> ");
+                carta = Integer.parseInt(myInput.readLine());
+            } catch (IOException ex) {
+                System.out.println("ahiahiaahi");
+            }
+            if(mano.get(carta).getType()=="Istant"){
+                playground.addStack(mano.get(carta-1));
+                mano.remove(carta-1);
+                return true;
+            }
+        }while((mano.get(carta).getType()!="Istant"));
+        return false;
+    }
+    
     public void giocaCarta(int indice) {
         //SI DEVE CONTROLLARE SE è MOSTRO O MAGIA CON IF THEN ELSE, ORA ADDO ALLA LISTA MOSTRI DI DEFAULT
-        playground.addMostro(mano.get(indice - 1), this);
+        if("Istant".equals(mano.get(indice-1).getType()) || "Enchantment".equals(mano.get(indice-1).getType())  || "Sourcery".equals(mano.get(indice-1).getType())){
+            addMagic(mano.get(indice-1));
+            playground.addStack(mano.get(indice-1));
+            playground.checkIstantOtherPlayer(this);
+        }
+        else{
+            addMonster(mano.get(indice-1));
+        }
+        //playground.addMostro(mano.get(indice - 1), this);
         mano.remove(indice - 1);
         //CODICE DI DEBUG
         playground.stampaMagie(playground.player1);
         playground.stampaMagie(playground.player2);
-
         playground.stampaMostri(playground.player1);
         playground.stampaMostri(playground.player2);
     }
