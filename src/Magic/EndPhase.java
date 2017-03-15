778d04d9494a4723be43d4857e4ba1d5f0222614
Enrico <Enrico@Enrico-PC>
2017-03-15 17:12:52 +0100

    Rimuove gli effetti delle magie in campo (non stregonerie che sono permanenti) e chiede invio per passare al turno successivo

--- src/Magic/EndPhase.java
+++ src/Magic/EndPhase.java
@@ -1,12 +1,26 @@
 package Magic;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  * Rappresnets the end phase
  *
  * @author Enrico
  */
 public class EndPhase implements Phase {
-
+    /**
+     * The input stream
+     */
+    private InputStreamReader reader;
+    
+    /**
+     * The buffer reader
+     */
+    private BufferedReader myInput;
     /**
      * The playground
      */
@@ -31,7 +45,20 @@ public class EndPhase implements Phase {
     @Override
     public void initPhase() {
         System.out.println("Sono nella End phase");
-        player.cleanMagicsOnGround();
+        //CONTROLLO LE MAGIE PRESENTI IN CAMPO E RIMUOVO TUTTI GLI EFFETTI TRANNE LE STREGONERIE
+        for(int i=0;i<player.getMagics().size();i++){
+            if(!"Sourcery".equals(player.getMagics().get(i).getType())){
+                player.getMagics().get(i).removeCard();
+            }
+        }
+        reader = new InputStreamReader(System.in);
+        myInput = new BufferedReader(reader);
+        System.out.println("Premere passare al prossimo turno.");
+        String c;
+        try {
+            c = myInput.readLine();
+        } catch (IOException ex) {
+            System.out.println("ahiahiahi");
+        }
     }
-
 }
