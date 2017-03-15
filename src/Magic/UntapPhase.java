4072c0687119c938bc27a81c2afe0283028f0df4
Enrico <Enrico@Enrico-PC>
2017-03-15 11:29:29 +0100

    Rimosso il codice per tappare i mostri (non andava qui, mannaggia)

--- src/Magic/UntapPhase.java
+++ src/Magic/UntapPhase.java
@@ -68,27 +68,31 @@ public class UntapPhase implements Phase {
                     player.getMonsters().get(i).setTapped(false);
                 }
             }
+        }/* CODICE PER TAPPARE I MOSTRI
             System.out.println(player.getNome() + " quali mostri vuoi tappare (indicare con un numero intero l'indice, -1 per non tappare nulla)?");
             for (int i = 0; i < player.getMonsters().size(); i++) {
                 System.out.print((i+1) + "-" + player.getMonsters().get(i).getName() + " ");
             }
-            try {
-                System.out.print("\n-> ");
-                int input = Integer.parseInt(myInput.readLine());
-                if (input != -1) {
-                    if ((input < 1) || (input > player.getMonsters().size() + 1)) {
-                        System.out.println("Indice errato");
-                    } else {
-                        //TAPPA IL MOSTRO INDICATO NEL CAMPO DI GIOCO
-                        player.tapMonster(input);
+            int input = 0;
+            do{
+                try {
+                    System.out.print("\n-> ");
+                    input = Integer.parseInt(myInput.readLine());
+                    if (input != -1) {
+                        if ((input < 1) || (input > player.getMonsters().size() + 1)) {
+                            System.out.print("Indice errato");
+                        } else {
+                            //TAPPA IL MOSTRO INDICATO NEL CAMPO DI GIOCO
+                            player.tapMonster(input-1);
+                        }
                     }
+                } catch (IOException ex) {
+                    System.out.println("Si è verificato un errore: " + ex);
                 }
-            } catch (IOException ex) {
-                System.out.println("Si è verificato un errore: " + ex);
-            }
-        }
+            }while(((input < 1) || (input > player.getMonsters().size()))&& input!=-1);
+        }*/
         else{
-            System.out.println("\nNon ci sono mostri da tappare.");
+            //System.out.println("\nNon ci sono mostri da tappare.");
         }
     }
 
